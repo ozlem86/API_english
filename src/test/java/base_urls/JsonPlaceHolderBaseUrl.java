191da598d5300987e28df2e4c15775ba2a966441
Ozlem Sert <ozlemksert@gmail.com>
2023-04-16 22:13:53 +0200

    Initial commit

---
+++ src/test/java/base_urls/JsonPlaceHolderBaseUrl.java
@@ -0,0 +1,16 @@
+package base_urls;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+public class JsonPlaceHolderBaseUrl {
+
+    protected RequestSpecification spec;
+
+    @Before//This method will run before each @Test methods.
+    public void setUp() {
+
+        spec = new RequestSpecBuilder().setContentType(ContentType.JSON).setBaseUri("https://jsonplaceholder.typicode.com").build();
+
+    }
+}
